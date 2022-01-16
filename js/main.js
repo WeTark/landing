0566cb9b7ccc34cec616e01e36c912ea6ec5ee67
Prabhat <prabhat.singh@navi.com>
2022-01-16 17:10:09 +0530

    price change

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let yesBar = document.querySelector('.yes-bar');
+let noBar = document.querySelector('.no-bar');
+let yesVal = document.querySelector('.bold-word-copy');    
+let noVal = document.querySelector('.bold-word-no-copy');   
+setInterval(oneSecondFunction, 500);
+function oneSecondFunction() {
+    var yesLen = 10*(yesBar.clientWidth)/(yesBar.clientWidth + noBar.clientWidth);
+    var yesLenInPoint = addZeroes(Math.round(yesLen*2)/2)
+    console.log(yesLenInPoint)
+    yesVal.innerHTML = `₹${yesLenInPoint}`
+    noVal.innerHTML = `₹${addZeroes(10-yesLenInPoint)}`
+}
+
+function addZeroes(num) {
+    return Number(num).toFixed(1)
+}
